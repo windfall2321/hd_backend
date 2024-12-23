6b5a30c7c86d36cb44d0a7080be3ceb317f96841
sxm <59765936+windfall2321@users.noreply.github.com>
2024-12-23 19:38:54 +0800

    修复fooditem信息缺少

--- src/main/java/com/hd/hd_backend/mapper/NotificationMapper.java
+++ src/main/java/com/hd/hd_backend/mapper/NotificationMapper.java
@@ -1,4 +1,17 @@
 package com.hd.hd_backend.mapper;
 
+
+import com.hd.hd_backend.entity.Notification;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
 public interface NotificationMapper {
+    List<Notification> getAllUserNotification(@Param("user_id")Integer userId);
+    List<Notification> getAllUserTypeNotification(@Param("user_id")Integer userId,Integer type);
+    Notification getNoticicationById(Integer id);
+    void insertNotification(Notification notification);
+    void updateNotification(Notification notification);
+    void deleteNotification(Integer id);
+
 }
