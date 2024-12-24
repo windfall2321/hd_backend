48734c7cc10d5d482ec440e3c16771725bcb885e
sxm <59765936+windfall2321@users.noreply.github.com>
2024-12-24 16:58:01 +0800

    handler的test

--- src/main/java/com/hd/hd_backend/utils/JsonUtils.java
+++ src/main/java/com/hd/hd_backend/utils/JsonUtils.java
@@ -4,6 +4,9 @@ public class JsonUtils {
     private static final ObjectMapper objectMapper = new ObjectMapper();
 
     public static String toJson(Object obj) {
+        if (obj == null) {
+            return null;
+        }
         try {
             return objectMapper.writeValueAsString(obj);
         } catch (Exception e) {
@@ -13,6 +16,9 @@ public class JsonUtils {
     }
 
     public static <T> T fromJson(String json, Class<T> clazz) {
+        if (json == null || clazz == null) {
+            return null;
+        }
         try {
             return objectMapper.readValue(json, clazz);
         } catch (Exception e) {
