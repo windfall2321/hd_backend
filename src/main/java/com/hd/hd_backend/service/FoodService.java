32ec3e18d34b4d5c0bcc4440d7c664a5de224da7
sxm <59765936+windfall2321@users.noreply.github.com>
2024-12-24 18:45:27 +0800

    加name

--- src/main/java/com/hd/hd_backend/service/FoodService.java
+++ src/main/java/com/hd/hd_backend/service/FoodService.java
@@ -1,5 +1,6 @@
 package com.hd.hd_backend.service;
 
+import com.hd.hd_backend.dto.FoodRecordDTO;
 import com.hd.hd_backend.entity.FoodItem;
 import com.hd.hd_backend.entity.FoodRecord;
 import java.util.List;
@@ -14,8 +15,8 @@ public interface FoodService {
     void deleteFoodItem(Integer foodId) throws Exception;
     
     // FoodRecord相关方法
-    List<FoodRecord> getFoodRecordsByUserId(int userId);
-    FoodRecord getFoodRecordById(int foodRecordId);
+    List<FoodRecordDTO> getFoodRecordsByUserId(int userId);
+    FoodRecordDTO getFoodRecordById(int foodRecordId);
     void addFoodRecord(FoodRecord foodRecord) throws Exception;
     void updateFoodRecord(FoodRecord foodRecord) throws Exception;
     void deleteFoodRecord(int foodRecordId) throws Exception;
