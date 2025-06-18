c1d9e9f39d61f98123a527342403f0b655253b53
sxm <59765936+windfall2321@users.noreply.github.com>
2025-06-18 22:24:19 +0800

    增加数据库的积分属性

--- src/main/java/com/hd/hd_backend/utils/APICaller.java
+++ src/main/java/com/hd/hd_backend/utils/APICaller.java
@@ -9,8 +9,7 @@ import java.util.Base64;
 import java.net.URLEncoder;
 
 public class APICaller {
-    private static final String API_KEY = "TwJ32wK5MVr9I6eQ9V9ANm7E";
-    private static final String SECRET_KEY = "UpqcVhFHbylg44iffY6PzXGhggsIEBeW";
+    private static final String token="Bearer bce-v3/ALTAK-zC7PA5H2pDDH5lLGBRXRa/5925c87e4cfb88a122f22811e5d1346bbc9b5727";
     private static final OkHttpClient HTTP_CLIENT = new OkHttpClient().newBuilder().build();
 
     /**
@@ -29,12 +28,16 @@ public class APICaller {
             
         RequestBody body = RequestBody.create(mediaType, requestBody);
 
-        // 构建请求
         Request request = new Request.Builder()
-                .url("https://aip.baidubce.com/rest/2.0/image-classify/v2/dish?access_token=" + getAccessToken())
+                .url("https://aip.baidubce.com/rest/2.0/image-classify/v2/dish")
                 .method("POST", body)
                 .addHeader("Content-Type", "application/x-www-form-urlencoded")
+                .addHeader("Authorization",token )
                 .build();
+
+
+        // 构建请求
+
 
         // 发送请求并获取响应
         try (Response response = HTTP_CLIENT.newCall(request).execute()) {
@@ -60,10 +63,12 @@ public class APICaller {
 
         // 构建请求
         Request request = new Request.Builder()
-                .url("https://aip.baidubce.com/rest/2.0/image-classify/v2/dish?access_token=" + getAccessToken())
+                .url("https://aip.baidubce.com/rest/2.0/image-classify/v2/dish")
                 .method("POST", body)
                 .addHeader("Content-Type", "application/x-www-form-urlencoded")
+                .addHeader("Authorization",token )
                 .build();
+
 
         // 发送请求并获取响应
         try (Response response = HTTP_CLIENT.newCall(request).execute()) {
@@ -86,23 +91,6 @@ public class APICaller {
         return base64;
     }
 
-    /**
-     * 从用户的AK，SK生成鉴权签名（Access Token）
-     * @return 鉴权签名（Access Token）
-     */
-    private static String getAccessToken() throws IOException {
-        MediaType mediaType = MediaType.parse("application/x-www-form-urlencoded");
-        RequestBody body = RequestBody.create(mediaType, "grant_type=client_credentials&client_id=" + API_KEY
-                + "&client_secret=" + SECRET_KEY);
-        Request request = new Request.Builder()
-                .url("https://aip.baidubce.com/oauth/2.0/token")
-                .method("POST", body)
-                .addHeader("Content-Type", "application/x-www-form-urlencoded")
-                .build();
-        try (Response response = HTTP_CLIENT.newCall(request).execute()) {
-            return new JSONObject(response.body().string()).getString("access_token");
-        }
-    }
 
     // 使用示例
 //    public static void main(String[] args) {
